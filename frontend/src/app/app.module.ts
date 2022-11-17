aad5a8201d5ce95af83bede889db13bdf109bcd1
keerthana121102 <101855404+keerthana121102@users.noreply.github.com>
2022-11-17 12:37:27 +0530

    error in task

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { NewListComponent } from './pages/new-list/new-list.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskViewComponent],
+  declarations: [AppComponent, TaskViewComponent, NewListComponent, NewTaskComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -21,30 +21,58 @@ export class TaskViewComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.taskService.getLists().subscribe((lists: any) => this.lists = lists);
+    this.taskService.getLists().subscribe((lists: any) => (this.lists = lists));
     // console.log('[+]Task list ',this.taskService);
     this.route.params.subscribe((params: Params) => {
-      console.log('[+]Task ',this.listId)
+      console.log('[+]Task ', this.listId);
       this.listId = params.listId;
       if (!this.listId) {
-        this.taskService.getAllTask().subscribe((tasks: any) => this.tasks = tasks);
+        this.taskService
+          .getAllTask()
+          .subscribe((tasks: any) => (this.tasks = tasks));
       }
       this.taskService
         .getTasks(this.listId)
         // .getAllTask()
-        .subscribe((tasks: any) => this.tasks = tasks);
-        console.log(this.listId);
-        
+        .subscribe((tasks: any) => (this.tasks = tasks));
+      console.log(this.listId);
     });
-  } 
-
-
-  onTaskClick(task: Task){
-      this.taskService.setCompleted(task._listId,task).subscribe(() => task.completed = !task.completed);
   }
-  onListClick(list:List){
-    console.log('[+]List clicked ',list)
-    this.listId=list._id
-    this.ngOnInit()
+
+  onTaskClick(task: Task) {
+    this.taskService
+      .setCompleted(task._listId, task)
+      .subscribe(() => (task.completed = !task.completed));
+  }
+
+  onListClick(list: List) {
+    console.log('[+]List clicked ', list);
+    this.listId = list._id;
+    this.ngOnInit();
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(this.listId, task._id)
+      .subscribe(
+        (task: any) =>
+          (this.tasks = this.tasks.filter((t) => t._id !== task._id))
+      );
+  }
+
+  deleteList(list: List) {
+    this.taskService
+      .deleteList(list._id)
+      .subscribe(
+        () => (this.lists = this.lists.filter((l) => l._id !== list._id))
+      );
+  }
+
+  addTaskClick() {
+    if (!this.listId) {
+      alert('Please select a list to add task');
+      return;
+    }
+    this.router.navigate(['./new-task'], { relativeTo: this.route });
   }
 }
